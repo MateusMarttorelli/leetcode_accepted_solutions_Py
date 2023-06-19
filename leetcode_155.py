77137f456e443dc1f4533e1ebc08666d04983fec
Mateus Marttorelli <mateus.hmarttorelli@gmail.com>
2023-06-19 20:29:39 -0300

    Updating

---
+++ leetcode_155.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = list()
+        self.minStack = list()
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if not self.minStack:
+            self.minStack.append(val)
+        else:
+            if val < self.minStack[-1]:
+                self.minStack.append(val)
+            else:
+                self.minStack.append(self.minStack[-1])
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return int(self.minStack[-1])
